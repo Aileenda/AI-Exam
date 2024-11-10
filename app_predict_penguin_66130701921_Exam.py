60e861b723156dd503e79d420735ae242713dca4
Aileenda <164203886+Aileenda@users.noreply.github.com>
2024-11-10 14:37:57 +0700

    Update app_predict_penguin_66130701921_Exam.py

--- app_predict_penguin_66130701921_Exam.py
+++ app_predict_penguin_66130701921_Exam.py
@@ -15,7 +15,7 @@ culmen_length = st.number_input('Culmen Length (mm)', min_value=0.0, max_value=1
 culmen_depth = st.number_input('Culmen Depth (mm)', min_value=0.0, max_value=100.0, value=19.3, step=0.1)
 flipper_length = st.number_input('Flipper Length (mm)', min_value=0.0, max_value=250.0, value=192.3, step=0.1)
 body_mass = st.number_input('Body Mass (g)', min_value=0.0, max_value=8000, value=3750.0, step=10.0)
-sex = st.selectbox('Sex', sex_encoder.classes_)
+sex = st.selectbox('Sex',  ['MALE','FEMALE'])
 
 # Preparing input for prediction
 x_new = pd.DataFrame({
